f1bcf834e9e055b9036b760fa44b567d013229e4
MinhTanTMT <132992493+MinhTanTMT@users.noreply.github.com>
2023-05-31 16:29:39 +0700

    tai data len

---
+++ SWP391/build/web/WEB-INF/classes/dal/DBContext.rs
@@ -0,0 +1,2 @@
+MySQLJDBCUtil
+DBContext
---
+++ SWP391/src/java/dal/DBContext.java
@@ -0,0 +1,28 @@
+package database;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DBContext {
+    
+    public static Connection getConnection() {
+        Connection c = null; 
+        try {
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver()); 
+            String url = "jdbc:mySQL://localhost:3306/swp391tmtnew";
+            String username = "root";
+            String password = "123456789?TM";
+                     
+            c = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return c;
+    }
+    public static void closeConnection(Connection c) {
+        
+    }
+}
